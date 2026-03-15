061188d442db603fcbbcd79107f55e7a8cf30df9
Friedrich von Never <friedrich@fornever.me>
2026-03-16 00:26:22 +0100

    CI: build before publishing

--- scripts/github-actions.fsx
+++ scripts/github-actions.fsx
@@ -135,6 +135,11 @@ let workflows = [
                 run = "echo \"version=$(scripts/Get-Version.ps1 -RefName $env:GITHUB_REF)\" >> $env:GITHUB_OUTPUT"
             )
             step(
+                name = "Build the project",
+                shell = "pwsh",
+                run = "dotnet build --configuration Release -p:Version=${{ steps.version.outputs.version }}"
+            )
+            step(
                 name = "Publish the project",
                 shell = "pwsh",
                 run = "dotnet publish --configuration Release -p:Version=${{ steps.version.outputs.version }}"
