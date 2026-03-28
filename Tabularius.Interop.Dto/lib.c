826327183cff0cd621cd88f1ca76641cda29966c
Friedrich von Never <friedrich@fornever.me>
2026-03-29 00:32:15 +0100

    Legal: specify the license for the new files

--- Tabularius.Interop.Dto/lib.c
+++ Tabularius.Interop.Dto/lib.c
@@ -1,2 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Friedrich von Never <friedrich@fornever.me>
+//
+// SPDX-License-Identifier: MIT
+
 // ReSharper disable once CppUnusedIncludeDirective — actually used for metadata generation
 #include "../HledgerInterop/include/verify_journal_result.h"
--- Tabularius.Interop/HledgerException.cs
+++ Tabularius.Interop/HledgerException.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Friedrich von Never <friedrich@fornever.me>
+//
+// SPDX-License-Identifier: MIT
+
 namespace Tabularius.Interop;
 
 public class HledgerException(string message, string stackTrace) : Exception
