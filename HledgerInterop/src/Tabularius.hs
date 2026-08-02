08b39e14c54488d3d1a1426579891d748cd1fbb6
Friedrich von Never <friedrich@fornever.me>
2026-08-02 23:19:41 +0200

    HledgerInterop: close the file after processing automatically

--- HledgerInterop/src/Tabularius.hs
+++ HledgerInterop/src/Tabularius.hs
@@ -8,11 +8,11 @@ import Data.Int (Int32)
 import Hledger.Data.Types (jtxns)
 import Hledger.Read (definputopts, orDieTrying, readJournal)
 import Hledger.Read.Common (PrefixedFilePath)
-import System.IO (IOMode (ReadMode), hSetEncoding, openFile, utf8)
+import System.IO (IOMode (ReadMode), hSetEncoding, utf8, withFile)
 
 verifyJournal :: PrefixedFilePath -> IO Int32
-verifyJournal path = do
-    h <- openFile path ReadMode
-    hSetEncoding h utf8
-    journal <- orDieTrying $ readJournal definputopts (Just path) h
-    return $ fromIntegral $ length $ jtxns journal
+verifyJournal path =
+    withFile path ReadMode $ \h -> do
+        hSetEncoding h utf8
+        journal <- orDieTrying $ readJournal definputopts (Just path) h
+        return $ fromIntegral $ length $ jtxns journal
